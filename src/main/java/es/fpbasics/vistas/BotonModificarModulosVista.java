f181725b28111eafcf68e3e62546ed62ae4d2566
JPimenton <javi.8894@gmail.com>
2018-06-04 18:32:27 +0200

    Update funcional

--- src/main/java/es/fpbasics/vistas/BotonModificarModulosVista.java
+++ src/main/java/es/fpbasics/vistas/BotonModificarModulosVista.java
@@ -27,31 +27,39 @@ public class BotonModificarModulosVista extends ApplicationContextHerency implem
 	public void init(){
 		
 		servicio = (ServicioModulos)this.context.getBean("servicioModulos");
+		nuevoRegistroModulos= new BotonModificarModulosVista();
 
 	}
 
 	public void botonParaUnaPrimaria(Integer idModulo) {
 		System.out.println(idModulo);
 		registroModulos = this.servicio.consultarRegistrosDeLaTablaModulosPorIdModulo(idModulo);
+		registroModulos.getIdModulo();
+		registroModulos.getNombre();
+		registroModulos.getDuracion();
+		registroModulos.getCurso();
 		System.out.println(registroModulos);
 		
 	}
 
-	public void generarNuevoObjetoAPartirDeRegistro (Integer idModulo,String nombre,Integer duracion,String curso) { 
-		nuevoRegistroModulos=new BotonModificarModulosVista();
-		nuevoRegistroModulos.setIdModulos(idModulo);
-		nuevoRegistroModulos.setNombre(nombre);
-		nuevoRegistroModulos.setDuracion(duracion);
-		nuevoRegistroModulos.setCurso(curso);
-			
-		System.out.println(nuevoRegistroModulos.getCurso());
+	public void generarNuevoObjetoAPartirDeRegistro () { 
+		nuevoRegistroModulos.setIdModulos(registroModulos.getIdModulo());
+		nuevoRegistroModulos.setNombre(this.nombre);
+		nuevoRegistroModulos.setDuracion(this.duracion);
+		nuevoRegistroModulos.setCurso(this.curso);
+		System.out.println(nuevoRegistroModulos.getIdModulos());
 		System.out.println(nuevoRegistroModulos.getNombre());
+		System.out.println(nuevoRegistroModulos.getDuracion());
+		System.out.println(nuevoRegistroModulos.getCurso());
 	}
 	
 		
 	public void confirmarModificacion() {
-		System.out.println("Entrando");
+		System.out.println("Entrando,este es el nuevo registro:");
+		System.out.println(nuevoRegistroModulos.getIdModulos());
 		System.out.println(nuevoRegistroModulos.getNombre());
+		System.out.println(nuevoRegistroModulos.getDuracion());
+		System.out.println(nuevoRegistroModulos.getCurso());
 		this.servicio.modificarRegistroDeLaTablaModulos(nuevoRegistroModulos);
 			
 		System.out.println("Actualizacion realizada con exito.");
@@ -59,6 +67,10 @@ public class BotonModificarModulosVista extends ApplicationContextHerency implem
 
 	public TablaModulos getRegistroModulos() {
 		return registroModulos;
+	}
+
+	public void setRegistroModulos(TablaModulos registroModulos) {
+		this.registroModulos = registroModulos;
 	}
 
 	public BotonModificarModulosVista getNuevoRegistroModulos() {
@@ -100,4 +112,6 @@ public class BotonModificarModulosVista extends ApplicationContextHerency implem
 	public void setCurso(String curso) {
 		this.curso = curso;
 	}
+
+		
 }
